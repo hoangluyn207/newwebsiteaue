8c78c9d2745da2a10f5c46d12a5486e38378da9d
Dang Hoang Luyn <hoangluyn207@gmail.com>
2023-03-31 10:11:13 +0700

    add

--- src/components/Project/ProjectContainer.js
+++ src/components/Project/ProjectContainer.js
@@ -12,7 +12,7 @@ import "./swiper.css";
 // import required modules
 import { Pagination } from "swiper";
 
-export default function ProjectContainer() {
+const ProjectContainer = () => {
   return (
     <Box padding={"128px 0px 0px 0px"}>
       <Swiper
@@ -34,11 +34,17 @@ export default function ProjectContainer() {
             backgroundColor="#FFF"
             alignItems="flex-start"
           >
-            <Image src="/image/project/lvs3.jpg" />
-            <Text>Title</Text>
+            <Box height="320px" width="100%" overflow={"hidden"}>
+              <Image
+                transition={"0.5s"}
+                src="/image/project/lvs3.jpg"
+                _hover={{ transform: "scale(1.2)" }}
+              />
+            </Box>
+            <Text padding={"8px 16px"}>Title</Text>
           </VStack>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide width={"full"}>
           <VStack
             width={"full"}
             height="full"
@@ -46,11 +52,17 @@ export default function ProjectContainer() {
             backgroundColor="#FFF"
             alignItems="flex-start"
           >
-            <Image src="/image/project/lvs3.jpg" />
-            <Text>Title</Text>
+            <Box height="320px" width="100%" overflow={"hidden"}>
+              <Image
+                transition={"0.5s"}
+                src="/image/project/lvs3.jpg"
+                _hover={{ transform: "scale(1.2)" }}
+              />
+            </Box>
+            <Text padding={"8px 16px"}>Title</Text>
           </VStack>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide width={"full"}>
           <VStack
             width={"full"}
             height="full"
@@ -58,10 +70,17 @@ export default function ProjectContainer() {
             backgroundColor="#FFF"
             alignItems="flex-start"
           >
-            <Image src="/image/project/lvs3.jpg" />
+            <Box height="320px" width="100%" overflow={"hidden"}>
+              <Image
+                transition={"0.5s"}
+                src="/image/project/lvs3.jpg"
+                _hover={{ transform: "scale(1.2)" }}
+              />
+            </Box>
+            <Text padding={"8px 16px"}>Title</Text>
           </VStack>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide width={"full"}>
           <VStack
             width={"full"}
             height="full"
@@ -69,10 +88,17 @@ export default function ProjectContainer() {
             backgroundColor="#FFF"
             alignItems="flex-start"
           >
-            <Image src="/image/project/lvs3.jpg" />
+            <Box height="320px" width="100%" overflow={"hidden"}>
+              <Image
+                transition={"0.5s"}
+                src="/image/project/lvs3.jpg"
+                _hover={{ transform: "scale(1.2)" }}
+              />
+            </Box>
+            <Text padding={"8px 16px"}>Title</Text>
           </VStack>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide width={"full"}>
           <VStack
             width={"full"}
             height="full"
@@ -80,54 +106,19 @@ export default function ProjectContainer() {
             backgroundColor="#FFF"
             alignItems="flex-start"
           >
-            <Image src="/image/project/lvs3.jpg" />
-          </VStack>
-        </SwiperSlide>
-        <SwiperSlide>
-          <VStack
-            width={"full"}
-            height="full"
-            justifyContent={"flex-start"}
-            backgroundColor="#FFF"
-            alignItems="flex-start"
-          >
-            <Image src="/image/project/lvs3.jpg" />
-          </VStack>
-        </SwiperSlide>
-        <SwiperSlide>
-          <VStack
-            width={"full"}
-            height="full"
-            justifyContent={"flex-start"}
-            backgroundColor="#FFF"
-            alignItems="flex-start"
-          >
-            <Image src="/image/project/lvs3.jpg" />
-          </VStack>
-        </SwiperSlide>
-        <SwiperSlide>
-          <VStack
-            width={"full"}
-            height="full"
-            justifyContent={"flex-start"}
-            backgroundColor="#FFF"
-            alignItems="flex-start"
-          >
-            <Image src="/image/project/lvs3.jpg" />
-          </VStack>
-        </SwiperSlide>
-        <SwiperSlide>
-          <VStack
-            width={"full"}
-            height="full"
-            justifyContent={"flex-start"}
-            backgroundColor="#FFF"
-            alignItems="flex-start"
-          >
-            <Image src="/image/project/lvs3.jpg" />
+            <Box height="320px" width="100%" overflow={"hidden"}>
+              <Image
+                transition={"0.5s"}
+                src="/image/project/lvs3.jpg"
+                _hover={{ transform: "scale(1.2)" }}
+              />
+            </Box>
+            <Text padding={"8px 16px"}>Title</Text>
           </VStack>
         </SwiperSlide>
       </Swiper>
     </Box>
   );
-}
+};
+
+export default ProjectContainer;
