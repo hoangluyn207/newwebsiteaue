e118b385f1cc7a60f1572f861ffed8ec188353b3
Dang Hoang Luyn <hoangluyn207@gmail.com>
2023-03-31 09:57:47 +0700

    fix

--- src/containers/hearder/Header.js
+++ src/containers/hearder/Header.js
@@ -4,10 +4,21 @@ import LogoAue from "../../image/LogoAue.png";
 import Menu from "../../components/Header/Menu";
 const Header = (props) => {
   return (
-    <Box {...props} width={"100%"} display={"flex"} justifyContent={"center"}>
+    <Box
+      position={"fixed"}
+      top="0"
+      zIndex={999}
+      {...props}
+      width={"100%"}
+      display={"flex"}
+      justifyContent={"center"}
+      backgroundColor="white"
+      margin={"0 !important"}
+      boxShadow="0 0 10px #cccccc"
+    >
       <HStack
         width={"1280px"}
-        height="56px"
+        height="84px"
         padding={"8px 32px"}
         display="flex"
         justifyContent={"space-between"}
